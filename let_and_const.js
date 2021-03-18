f005a434e16b718ac83b78f72e4f985c27d0d8b5
islamdiab-stack <islamdiab967@gmail.com>
2021-03-18 12:32:03 +0200

    Add new challenge on let and const

---
+++ let_and_const.js
@@ -0,0 +1,32 @@
+/*
+Task
+
+1- Declare a constant variable, PI , and assign it the value Math.PI. 
+You will not pass this challenge unless the variable is declared as a constant and named PI (uppercase).
+2- Read a number, r, denoting the radius of a circle from stdin.
+3- Use PI and r to calculate the area and perimeter of a circle having radius r.
+4- Print area as the first line of output and print perimeter as the second line of output.
+
+Input Format
+A single integer, r, denoting the radius of a circle.
+
+Constraints
+0 < r <= 100
+r is a floating-point number scaled to at most 3 decimal places.
+
+Output Format
+
+Print the following two lines:
+1- On the first line, print the area of the circle having radius r.
+2- On the second line, print the perimeter of the circle having radius r.
+
+
+Explanation 0
+Given the radius r = 2.6 , we calculate the following:
+area = Pi * r^2 = 21.237166338267002
+perimeter = 2 * pi * r = 16.336281798666924
+We then print area as our first line of output and perimeter as our second line of output.
+
+*/
+
+
